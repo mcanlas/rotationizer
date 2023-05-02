d3fb1d6e38c7f9a27b0b46c29c7465f1451226f7
Mark Canlas <github@htmlism.com>
2023-05-02 19:35:27 -0400

    foundations :hut:

---
+++ build.sbt
@@ -0,0 +1,15 @@
+lazy val `rotationizer` =
+  project
+    .in(file("."))
+    .aggregate(core, solver, demo)
+
+lazy val core =
+  module("core").withTesting
+
+lazy val solver =
+  module("solver").withTesting
+
+lazy val demo =
+  module("demo")
+    .dependsOn(core, solver)
+    .withTesting
---
+++ project/ProjectPlugin.scala
@@ -0,0 +1,46 @@
+import sbt.Keys._
+import sbt._
+
+/**
+  * Automatically enriches projects with the following settings (despite the word "override").
+  */
+object ProjectPlugin extends AutoPlugin {
+
+  /**
+    * Defines what members will be imported to the `build.sbt` scope.
+    */
+  val autoImport = ThingsToAutoImport
+
+  /**
+    * Thus plug-in will automatically be enabled; it has no requirements.
+    */
+  override def trigger: PluginTrigger = AllRequirements
+
+  object ThingsToAutoImport {
+    private def jarName(s: String) =
+      "rotationizer-" + s
+
+    def module(s: String): Project =
+      Project(s, file(jarName(s)))
+        .settings(name := jarName(s))
+
+    implicit class ProjectOps(p: Project) {
+      def withEffectMonad: Project =
+        p
+          .settings(libraryDependencies += "org.typelevel" %% "cats-effect" % "3.4.10")
+
+      def withTesting: Project = {
+        val weaverVersion =
+          "0.8.3"
+
+        p.settings(
+          testFrameworks += new TestFramework("weaver.framework.CatsEffect"),
+          libraryDependencies ++= Seq(
+            "com.disneystreaming" %% "weaver-cats"       % weaverVersion % Test,
+            "com.disneystreaming" %% "weaver-scalacheck" % weaverVersion % Test
+          )
+        )
+      }
+    }
+  }
+}
