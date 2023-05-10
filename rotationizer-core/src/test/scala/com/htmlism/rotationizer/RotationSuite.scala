e8797ab093704a15f530072aabc6a49e69868675
Mark Canlas <github@htmlism.com>
2023-05-10 17:29:42 -0400

    factor out court generation :factory:

--- rotationizer-core/src/test/scala/com/htmlism/rotationizer/RotationSuite.scala
+++ rotationizer-core/src/test/scala/com/htmlism/rotationizer/RotationSuite.scala
@@ -9,13 +9,12 @@ import com.htmlism.rotationizer.gen.given
 object RotationSuite extends SimpleIOSuite with Checkers:
   test("A rotation 6 generates 6 cycles") {
     forall { (r: Rotation.Rotation6) =>
+      val courts =
+        Rotation
+          .courts(r)
+
       val numCyclesEqualsRosterSize =
-        expect.eql(
-          6,
-          Rotation
-            .courts(r)
-            .length
-        )
+        expect.eql(6, courts.length)
 
       numCyclesEqualsRosterSize
     }
@@ -23,13 +22,12 @@ object RotationSuite extends SimpleIOSuite with Checkers:
 
   test("A rotation 7 generates 7 cycles") {
     forall { (r: Rotation.Rotation7) =>
+      val courts =
+        Rotation
+          .courts(r)
+
       val numCyclesEqualsRosterSize =
-        expect.eql(
-          7,
-          Rotation
-            .courts(r)
-            .length
-        )
+        expect.eql(7, courts.length)
 
       numCyclesEqualsRosterSize
     }
