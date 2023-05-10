5b860d4fa00a7cee5f38aa9616fdd841328b8c71
Mark Canlas <github@htmlism.com>
2023-05-10 12:42:13 -0400

    add court model :tennis:

---
+++ rotationizer-core/src/main/scala/com/htmlism/rotationizer/Court.scala
@@ -0,0 +1,25 @@
+package com.htmlism.rotationizer
+
+import cats.data.*
+
+case class Court[A](xs: NonEmptyList[A], serverSideOuts: List[A], hitterSideOuts: List[A]):
+  private lazy val xsl =
+    xs.toList
+
+  def position1: A =
+    xsl(0)
+
+  def position2: A =
+    xsl(1)
+
+  def position3: A =
+    xsl(2)
+
+  def position4: A =
+    xsl(3)
+
+  def position5: A =
+    xsl(4)
+
+  def position6: A =
+    xsl(5)
