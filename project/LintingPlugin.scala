237e58e5cfcd20f113f4b8fb2da951d9be4a747d
Mark Canlas <github@htmlism.com>
2023-05-12 08:05:08 -0400

    organize

--- project/LintingPlugin.scala
+++ project/LintingPlugin.scala
@@ -3,7 +3,8 @@ import sbt._
 import scalafix.sbt.ScalafixPlugin.autoImport._
 
 object LintingPlugin extends AutoPlugin {
-  override def trigger = allRequirements
+  override def trigger =
+    allRequirements
 
   override val globalSettings =
     addCommandAlias("fmt", "; scalafmtSbt; scalafmtAll") ++
