ad77cd1d4bb2c5608190fd0d057d92f604ed7d63
Mark Canlas <github@htmlism.com>
2023-05-23 16:54:13 -0400

    add todo :parasol_on_ground:

---
+++ rotationizer-core/src/test/scala/com/htmlism/rotationizer/CourtSuite.scala
@@ -0,0 +1,11 @@
+package com.htmlism.rotationizer
+
+import weaver._
+
+object CourtSuite extends FunSuite:
+  // TODO
+  test("a court has a setter") {
+    implicitly[RandomInstance[Rotation.Rotation6]]
+
+    expect.eql(1, 1)
+  }
--- rotationizer-core/src/test/scala/com/htmlism/rotationizer/RandomInstanceSuite.scala
+++ rotationizer-core/src/test/scala/com/htmlism/rotationizer/RandomInstanceSuite.scala
@@ -3,12 +3,14 @@ package com.htmlism.rotationizer
 import weaver._
 
 object RandomInstanceSuite extends FunSuite:
+  // TODO
   test("supports a rotation of six players") {
     implicitly[RandomInstance[Rotation.Rotation6]]
 
     expect.eql(1, 1)
   }
 
+  // TODO
   test("supports a rotation of seven players") {
     implicitly[RandomInstance[Rotation.Rotation7]]
 
