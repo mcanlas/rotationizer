37456ab4acdb3e39982c37405249cfc50ab81b2f
Mark Canlas <github@htmlism.com>
2024-03-11 12:44:00 -0400

    cats-effect 3.5.4

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -27,7 +27,7 @@ object ProjectPlugin extends AutoPlugin {
     implicit class ProjectOps(p: Project) {
       def withEffectMonad: Project =
         p
-          .settings(libraryDependencies += "org.typelevel" %% "cats-effect" % "3.5.3")
+          .settings(libraryDependencies += "org.typelevel" %% "cats-effect" % "3.5.4")
 
       def withTesting: Project = {
         val weaverVersion =
