f466296f3d5dffb60e45a2eb79d62dbabc9a8046
Mark Canlas <github@htmlism.com>
2024-05-17 13:19:43 -0400

    drop unused

--- rotationizer-core/src/test/scala/com/htmlism/rotationizer/CourtSuite.scala
+++ rotationizer-core/src/test/scala/com/htmlism/rotationizer/CourtSuite.scala
@@ -5,6 +5,4 @@ import weaver.*
 object CourtSuite extends FunSuite:
   // TODO
   test("a court has a setter"):
-    implicitly[RandomInstance[Rotation.Rotation6]]
-
     expect.eql(1, 1)
--- rotationizer-core/src/test/scala/com/htmlism/rotationizer/RandomInstanceSuite.scala
+++ rotationizer-core/src/test/scala/com/htmlism/rotationizer/RandomInstanceSuite.scala
@@ -5,12 +5,8 @@ import weaver.*
 object RandomInstanceSuite extends FunSuite:
   // TODO
   test("supports a rotation of six players"):
-    implicitly[RandomInstance[Rotation.Rotation6]]
-
     expect.eql(1, 1)
 
   // TODO
   test("supports a rotation of seven players"):
-    implicitly[RandomInstance[Rotation.Rotation7]]
-
     expect.eql(1, 1)
